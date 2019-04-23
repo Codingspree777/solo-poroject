c282328fb21648e6966b3d5225e1566859a7a276
Mike Huynh <hades@mikes-mbp.home>
2019-04-22 23:18:21 -0700

    commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// uncomment the below for proxy challenge
+
+const leaderList = [
+  {name: 'Anna', id: 'a0'},
+  {name: 'Ben', id: 'b0'},
+  {name: 'Clara', id: 'c0'},
+  {name: 'David', id: 'd0'},
+];
+
+app.get('/api/leaders/', (req, res) => {
+  res.send(leaderList);
+});
+
+//console.log(process.env)
+//console.log(process.env.npm_package_scripts_build)
+console.log(process.env.NODE_ENV);
+
+// statically serve everything in the build folder on the route '/build'
+if(process.env.NODE_ENV === 'production')
+//if(process.env.npm_package_scripts_build === 'NODE_ENV=production webpack')
+{
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+  // serve index.html on the route '/'
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+}
+app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: './client/index.js',
+    devServer: {
+        publicPath: 'http://localhost:8080/build/',
+        port: 8080,
+        proxy: {'/api/leaders/': 'http://localhost:3000'}
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [ 
+        {
+          test: /\.jsx?/,
+          exclude: '/(node_modules)/',
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          },
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        }
+      ]
+    }
+}
+
